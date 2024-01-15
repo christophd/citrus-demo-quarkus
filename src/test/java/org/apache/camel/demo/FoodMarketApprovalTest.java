70ff90de4f0110c6d35344d117b1268380ada182
Christoph Deppisch <cdeppisch@redhat.com>
2024-01-15 12:51:15 +0100

    Citrus Quarkus Demo

---
+++ src/test/java/org/apache/camel/demo/FoodMarketApprovalTest.java
@@ -0,0 +1,162 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.camel.demo;
+
+import javax.sql.DataSource;
+
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import org.apache.camel.demo.behavior.VerifyBookingCompletedMail;
+import org.apache.camel.demo.behavior.VerifyBookingStatus;
+import org.apache.camel.demo.model.Booking;
+import org.apache.camel.demo.model.Product;
+import org.apache.camel.demo.model.Supply;
+import org.apache.camel.demo.model.event.BookingCompletedEvent;
+import org.apache.camel.demo.model.event.ShippingEvent;
+import org.citrusframework.TestCaseRunner;
+import org.citrusframework.annotations.CitrusConfiguration;
+import org.citrusframework.annotations.CitrusEndpoint;
+import org.citrusframework.annotations.CitrusResource;
+import org.citrusframework.http.client.HttpClient;
+import org.citrusframework.kafka.endpoint.KafkaEndpoint;
+import org.citrusframework.mail.server.MailServer;
+import org.citrusframework.quarkus.CitrusSupport;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
+import static org.citrusframework.actions.ReceiveMessageAction.Builder.receive;
+import static org.citrusframework.dsl.JsonSupport.json;
+import static org.citrusframework.dsl.JsonSupport.marshal;
+import static org.citrusframework.http.actions.HttpActionBuilder.http;
+
+@QuarkusTest
+@CitrusSupport
+@CitrusConfiguration(classes = { CitrusEndpointConfig.class })
+class FoodMarketApprovalTest {
+
+    @CitrusEndpoint
+    HttpClient foodMarketApiClient;
+
+    @CitrusEndpoint
+    KafkaEndpoint completed;
+
+    @CitrusEndpoint
+    KafkaEndpoint shipping;
+
+    @CitrusEndpoint
+    MailServer mailServer;
+
+    @CitrusResource
+    TestCaseRunner t;
+
+    @Inject
+    DataSource dataSource;
+
+    @Test
+    void shouldRequireApproval() {
+        Product product = new Product("Kiwi");
+
+        Supply supply = new Supply("citrus-test", product, 200, 1.99D);
+        createSupply(supply);
+
+        Booking booking = new Booking("citrus-test", product, 200, 1.99D, TestHelper.createShippingAddress().getFullAddress());
+        createBooking(booking);
+
+        t.then(t.applyBehavior(new VerifyBookingStatus(Booking.Status.APPROVAL_REQUIRED, dataSource)));
+
+        approveBooking();
+
+        BookingCompletedEvent completedEvent = BookingCompletedEvent.from(booking);
+        verifyBookingCompletedEvent(completedEvent);
+
+        ShippingEvent shippingEvent = new ShippingEvent(booking.getClient(), product.getName(),
+                supply.getAmount(), booking.getShippingAddress());
+        verifyShippingEvent(shippingEvent);
+
+        t.then(t.applyBehavior(new VerifyBookingCompletedMail(booking, mailServer)));
+
+        t.then(t.applyBehavior(new VerifyBookingStatus(Booking.Status.COMPLETED, dataSource)));
+    }
+
+    private void createBooking(Booking booking) {
+        t.variable("booking", booking);
+        t.when(http()
+                .client(foodMarketApiClient)
+                .send()
+                .post("/api/bookings")
+                .message()
+                .contentType(APPLICATION_JSON)
+                .body(marshal(booking)));
+
+        t.then(http()
+                .client(foodMarketApiClient)
+                .receive()
+                .response(HttpStatus.CREATED)
+                .message()
+                .extract(json().expression("$.id", "bookingId")));
+    }
+
+    private void createSupply(Supply supply) {
+        t.when(http()
+                .client(foodMarketApiClient)
+                .send()
+                .post("/api/supplies")
+                .message()
+                .contentType(APPLICATION_JSON)
+                .body(marshal(supply)));
+
+        t.then(http()
+                .client(foodMarketApiClient)
+                .receive()
+                .response(HttpStatus.CREATED)
+                .message()
+                .extract(json().expression("$.id", "supplyId")));
+    }
+
+    private void verifyShippingEvent(ShippingEvent shippingEvent) {
+        t.then(receive()
+                .endpoint(shipping)
+                .message().body(marshal(shippingEvent)));
+    }
+
+    private void verifyBookingCompletedEvent(BookingCompletedEvent completedEvent) {
+        t.then(receive()
+                .endpoint(completed)
+                .message().body(marshal(completedEvent)));
+    }
+
+    /**
+     * Approve booking by calling the FoodMarket Http REST API.
+     * Uses a test variable bookingId to identify the booking to approve.
+     * The bookingId usually gets extracted from the response when creating the booking in an earlier step in this test.
+     */
+    private void approveBooking() {
+        t.when(http()
+                .client(foodMarketApiClient)
+                .send()
+                .put("/api/bookings/approval/${bookingId}")
+                .message());
+
+        t.then(http()
+                .client(foodMarketApiClient)
+                .receive()
+                .response(HttpStatus.ACCEPTED));
+    }
+
+}
---
+++ src/test/java/org/apache/camel/demo/FoodMarketEventingTest.java
@@ -0,0 +1,212 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.camel.demo;
+
+import javax.sql.DataSource;
+
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import org.apache.camel.demo.behavior.GetShippingAddress;
+import org.apache.camel.demo.behavior.VerifyBookingCompletedMail;
+import org.apache.camel.demo.behavior.WaitForEntityPersisted;
+import org.apache.camel.demo.behavior.WaitForProductCreated;
+import org.apache.camel.demo.model.Booking;
+import org.apache.camel.demo.model.Product;
+import org.apache.camel.demo.model.ShippingAddress;
+import org.apache.camel.demo.model.Supply;
+import org.apache.camel.demo.model.event.BookingCompletedEvent;
+import org.apache.camel.demo.model.event.ShippingEvent;
+import org.citrusframework.TestCaseRunner;
+import org.citrusframework.annotations.CitrusConfiguration;
+import org.citrusframework.annotations.CitrusEndpoint;
+import org.citrusframework.annotations.CitrusResource;
+import org.citrusframework.http.server.HttpServer;
+import org.citrusframework.kafka.endpoint.KafkaEndpoint;
+import org.citrusframework.mail.server.MailServer;
+import org.citrusframework.quarkus.CitrusSupport;
+import org.junit.jupiter.api.Test;
+
+import static org.citrusframework.actions.ReceiveMessageAction.Builder.receive;
+import static org.citrusframework.actions.SendMessageAction.Builder.send;
+import static org.citrusframework.actions.SleepAction.Builder.delay;
+import static org.citrusframework.container.Iterate.Builder.iterate;
+import static org.citrusframework.container.Parallel.Builder.parallel;
+import static org.citrusframework.dsl.JsonSupport.marshal;
+
+@QuarkusTest
+@CitrusSupport
+@CitrusConfiguration(classes = { CitrusEndpointConfig.class })
+class FoodMarketEventingTest {
+
+    @CitrusEndpoint
+    KafkaEndpoint products;
+
+    @CitrusEndpoint
+    KafkaEndpoint bookings;
+
+    @CitrusEndpoint
+    KafkaEndpoint supplies;
+
+    @CitrusEndpoint
+    KafkaEndpoint completed;
+
+    @CitrusEndpoint
+    KafkaEndpoint shipping;
+
+    @CitrusEndpoint
+    MailServer mailServer;
+
+    @CitrusEndpoint
+    HttpServer shippingDetailsService;
+
+    @CitrusResource
+    TestCaseRunner t;
+
+    @Inject
+    DataSource dataSource;
+
+    @Test
+    void shouldCompleteOnBooking() {
+        Product product = new Product("Pineapple");
+
+        Supply supply = new Supply("citrus-test", product, 100, 0.90D);
+        createSupply(supply);
+
+        t.then(t.applyBehavior(new WaitForEntityPersisted(supply, dataSource)));
+
+        Booking booking = new Booking("citrus-test", product, 100, 0.99D);
+        createBooking(booking);
+
+        BookingCompletedEvent completedEvent = BookingCompletedEvent.from(booking);
+        verifyBookingCompletedEvent(completedEvent);
+
+        ShippingAddress shippingAddress = TestHelper.createShippingAddress();
+        ShippingEvent shippingEvent = new ShippingEvent(booking.getClient(), product.getName(),
+                supply.getAmount(), shippingAddress.getFullAddress());
+
+        t.then(t.applyBehavior(new GetShippingAddress(shippingDetailsService, shippingAddress)));
+
+        verifyShippingEvent(shippingEvent);
+
+        t.then(t.applyBehavior(new VerifyBookingCompletedMail(booking, mailServer)));
+    }
+
+    @Test
+    void shouldCompleteOnSupply() {
+        Product product = new Product("Watermelon");
+        createProduct(product);
+
+        t.then(t.applyBehavior(new WaitForProductCreated(product, dataSource)));
+
+        Booking booking = new Booking("citrus-test", product, 100, 0.99D);
+        createBooking(booking);
+
+        t.then(t.applyBehavior(new WaitForEntityPersisted(booking, dataSource)));
+
+        Supply supply = new Supply("citrus-test", product, 100, 0.99D);
+        createSupply(supply);
+
+        BookingCompletedEvent completedEvent = BookingCompletedEvent.from(booking);
+        verifyBookingCompletedEvent(completedEvent);
+
+        ShippingAddress shippingAddress = TestHelper.createShippingAddress();
+        ShippingEvent shippingEvent = new ShippingEvent(booking.getClient(), product.getName(),
+                supply.getAmount(), shippingAddress.getFullAddress());
+
+        t.then(t.applyBehavior(new GetShippingAddress(shippingDetailsService, shippingAddress)));
+
+        verifyShippingEvent(shippingEvent);
+
+        t.then(t.applyBehavior(new VerifyBookingCompletedMail(booking, mailServer)));
+    }
+
+    private void verifyShippingEvent(ShippingEvent shippingEvent) {
+        t.then(receive()
+                .endpoint(shipping)
+                .message().body(marshal(shippingEvent)));
+    }
+
+    private void verifyBookingCompletedEvent(BookingCompletedEvent completedEvent) {
+        t.then(receive()
+                .endpoint(completed)
+                .message().body(marshal(completedEvent)));
+    }
+
+    private void createSupply(Supply supply) {
+        t.when(send()
+                .endpoint(supplies)
+                .message().body(marshal(supply)));
+    }
+
+    private void createBooking(Booking booking) {
+        t.variable("booking", booking);
+        t.when(send()
+                .endpoint(bookings)
+                .message().body(marshal(booking)));
+    }
+
+    private void createProduct(Product product) {
+        t.when(send()
+                .endpoint(products)
+                .message().body(marshal(product)));
+    }
+
+    @Test
+    void shouldCompleteAllMatchingBookings() {
+        Product product = new Product("Apple");
+        t.variable("product", product);
+
+        Booking booking = new Booking("citrus-test", product, 10, 1.99D, TestHelper.createShippingAddress().getFullAddress());
+        t.when(iterate()
+                .condition((i, context) -> i < 10)
+                .actions(
+                        send()
+                            .endpoint(bookings)
+                            .message().body(marshal(booking))));
+        t.variable("booking", booking);
+
+        t.$(delay().milliseconds(1000L));
+
+        Supply supply = new Supply("citrus-test", product, 100, 0.99D);
+        t.then(send()
+                .endpoint(supplies)
+                .message().body(marshal(supply)));
+
+        BookingCompletedEvent completedEvent = BookingCompletedEvent.from(booking);
+        ShippingEvent shippingEvent = new ShippingEvent(booking.getClient(), product.getName(),
+                booking.getAmount(), booking.getShippingAddress());
+
+        t.then(parallel().actions(
+            iterate()
+                .condition((i, context) -> i < 10)
+                .actions(
+                    receive()
+                        .endpoint(completed)
+                        .message().body(marshal(completedEvent))
+                ),
+            iterate()
+                .condition((i, context) -> i < 10)
+                .actions(
+                    receive()
+                        .endpoint(shipping)
+                        .message().body(marshal(shippingEvent))
+                )
+        ));
+    }
+
+}
