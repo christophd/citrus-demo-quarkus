15ffe2e4006dccd0adb5369e73c6d65a94ae9ba1
Christoph Deppisch <cdeppisch@redhat.com>
2024-01-16 09:26:23 +0100

    Stage D - Mail verification
    - Test adds verification for the booking completed mail sent by the system under test

--- src/test/java/org/apache/camel/demo/FoodMarketDemoTest.java
+++ src/test/java/org/apache/camel/demo/FoodMarketDemoTest.java
@@ -1,8 +1,6 @@
 package org.apache.camel.demo;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.quarkus.test.junit.QuarkusTest;
-import jakarta.inject.Inject;
 import org.apache.camel.demo.model.Booking;
 import org.apache.camel.demo.model.Product;
 import org.apache.camel.demo.model.Supply;
@@ -13,13 +11,14 @@ import org.citrusframework.annotations.CitrusConfiguration;
 import org.citrusframework.annotations.CitrusEndpoint;
 import org.citrusframework.annotations.CitrusResource;
 import org.citrusframework.kafka.endpoint.KafkaEndpoint;
+import org.citrusframework.mail.message.MailMessage;
+import org.citrusframework.mail.server.MailServer;
 import org.citrusframework.quarkus.CitrusSupport;
 import org.junit.jupiter.api.Test;
 
 import static org.citrusframework.actions.ReceiveMessageAction.Builder.receive;
 import static org.citrusframework.actions.SendMessageAction.Builder.send;
 import static org.citrusframework.dsl.JsonSupport.marshal;
-import static org.citrusframework.kafka.endpoint.builder.KafkaEndpoints.kafka;
 
 @QuarkusTest
 @CitrusSupport
@@ -30,6 +29,9 @@ public class FoodMarketDemoTest {
     TestCaseRunner t;
 
     @CitrusEndpoint
+    KafkaEndpoint bookings;
+
+    @CitrusEndpoint
     KafkaEndpoint supplies;
 
     @CitrusEndpoint
@@ -38,15 +40,18 @@ public class FoodMarketDemoTest {
     @CitrusEndpoint
     KafkaEndpoint shipping;
 
+    @CitrusEndpoint
+    MailServer mailServer;
+
     @Test
     void shouldMatchBookingAndSupply() {
         Product product = new Product("Kiwi");
-        Booking booking = new Booking("citrus", product, 10, 0.99D,
+        Booking booking = new Booking("citrus", product, 100, 0.99D,
                 TestHelper.createShippingAddress().getFullAddress());
 
         createBooking(booking);
 
-        Supply supply = new Supply("citrus", product, 10, 0.99D);
+        Supply supply = new Supply("citrus", product, 100, 0.99D);
         createSupply(supply);
 
         BookingCompletedEvent bookingCompletedEvent = BookingCompletedEvent.from(booking);
@@ -55,14 +60,28 @@ public class FoodMarketDemoTest {
         ShippingEvent shippingEvent = new ShippingEvent(booking.getClient(), product.getName(),
                 booking.getAmount(), booking.getShippingAddress());
         verifyShippingEvent(shippingEvent);
+
+        verifyBookingCompletedMail(booking);
+    }
+
+    private void verifyBookingCompletedMail(Booking booking) {
+        t.then(receive()
+                .endpoint(mailServer)
+                .message(MailMessage.request()
+                        .from("foodmarket@quarkus.io")
+                        .to("%s@quarkus.io".formatted(booking.getClient()))
+                        .subject("Booking completed!")
+                        .body("Hey %s, your booking %s has been completed."
+                                .formatted(booking.getClient(), booking.getProduct().getName()), "text/plain")));
+
+        t.then(send()
+                .endpoint(mailServer)
+                .message(MailMessage.response(250)));
     }
 
     private void createBooking(Booking booking) {
         t.when(send()
-                .endpoint(kafka()
-                        .asynchronous()
-                        .topic("bookings")
-                        .build())
+                .endpoint(bookings)
                 .message()
                 .body(marshal(booking))
         );
